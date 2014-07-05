15cb4b7f26292f962851c9ef04c7b0a8e2642905
Andy <acromace@gmail.com>
2014-07-05 18:56:28 -0400

    Added ability to use the other eye emoticons

--- web/controller/page-eyes.js
+++ web/controller/page-eyes.js
@@ -2,7 +2,16 @@ App.controller('page-eyes', function (page) {
 	$(page).find('#button-eye-1').click(function () {
 		kik.send({
 			title: 'ಠ_ಠ'
-			// text: 'ಠ_ಠ'
+		});
+	});
+	$(page).find('#button-eye-2').click(function () {
+		kik.send({
+			title: 'ಠ◡ಠ'
+		});
+	});
+	$(page).find('#button-eye-3').click(function () {
+		kik.send({
+			title: '◕‿◕'
 		});
 	});
 });
