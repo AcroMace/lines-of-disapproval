76d37d3487145f3e8c37554c0de4d9f68ec6aef9
Andy <acromace@gmail.com>
2014-07-05 21:18:07 -0400

    Added arm emoticons

---
+++ web/controller/page-arms.js
@@ -0,0 +1,52 @@
+App.controller('page-arms', function (page) {
+	$(page).find('#button-arm-1').click(function () {
+		kik.send({
+			title: 'ᕕ( ᐛ )ᕗ'
+		});
+	});
+	$(page).find('#button-arm-2').click(function () {
+		kik.send({
+			title: '¯\\_(ツ)_/¯'
+		});
+	});
+	$(page).find('#button-arm-3').click(function () {
+		kik.send({
+			title: '(╯°□°）╯︵ ┻━┻'
+		});
+	});
+	$(page).find('#button-arm-4').click(function () {
+		kik.send({
+			title: '┬─┬ノ( º _ ºノ)'
+		});
+	});
+	$(page).find('#button-arm-5').click(function () {
+		kik.send({
+			title: '(☞ﾟ∀ﾟ)☞'
+		});
+	});
+	$(page).find('#button-arm-6').click(function () {
+		kik.send({
+			title: '☜(ﾟヮﾟ☜)'
+		});
+	});
+	$(page).find('#button-arm-7').click(function () {
+		kik.send({
+			title: 'ヽ༼ຈل͜ຈ༽ﾉ'
+		});
+	});
+	$(page).find('#button-arm-8').click(function () {
+		kik.send({
+			title: '༼ つ ◕_◕ ༽つ'
+		});
+	});
+	$(page).find('#button-arm-9').click(function () {
+		kik.send({
+			title: 'ლ(ಠ益ಠლ)'
+		});
+	});
+	$(page).find('#button-arm-10').click(function () {
+		kik.send({
+			title: '(ง\'̀-\'́)ง'
+		});
+	});
+});
