200a8dee91dd8eff93ad3403fb9c0b8664aedb01
Andy <acromace@gmail.com>
2014-07-05 22:17:48 -0400

    Added character emoticons

---
+++ controller/page-chars.js
@@ -0,0 +1,22 @@
+App.controller('page-chars', function (page) {
+	$(page).find('#button-char-1').click(function () {
+		kik.send({
+			title: '| (• ◡•)|'
+		});
+	});
+	$(page).find('#button-char-2').click(function () {
+		kik.send({
+			title: '(❍ᴥ❍ʋ)'
+		});
+	});
+	$(page).find('#button-char-3').click(function () {
+		kik.send({
+			title: 'ʕ•ᴥ•ʔ'
+		});
+	});
+	$(page).find('#button-char-4').click(function () {
+		kik.send({
+			title: '(V) (;,,;) (V)'
+		});
+	});
+});
