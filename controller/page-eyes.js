b341887b7aa6560cbb0449be3f5372b619d636a6
Andy <acromace@gmail.com>
2014-07-05 21:18:07 -0400

    Added arm emoticons

--- controller/page-eyes.js
+++ controller/page-eyes.js
@@ -14,4 +14,39 @@ App.controller('page-eyes', function (page) {
 			title: '◕‿◕'
 		});
 	});
+	$(page).find('#button-eye-4').click(function () {
+		kik.send({
+			title: 'ʘ‿ʘ'
+		});
+	});
+	$(page).find('#button-eye-5').click(function () {
+		kik.send({
+			title: '♥‿♥'
+		});
+	});
+	$(page).find('#button-eye-6').click(function () {
+		kik.send({
+			title: '◔̯◔'
+		});
+	});
+	$(page).find('#button-eye-7').click(function () {
+		kik.send({
+			title: 'ಥ_ಥ'
+		});
+	});
+	$(page).find('#button-eye-8').click(function () {
+		kik.send({
+			title: '͡° ͜ʖ ͡°'
+		});
+	});
+	$(page).find('#button-eye-9').click(function () {
+		kik.send({
+			title: '◕‿↼'
+		});
+	});
+	$(page).find('#button-eye-10').click(function () {
+		kik.send({
+			title: '•_•'
+		});
+	});
 });
