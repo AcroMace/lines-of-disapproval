1f8009362cc10f06e521f553160b6eb1fe171846
Andy <acromace@gmail.com>
2014-07-06 16:41:11 -0400

    Added others and more emoticons

---
+++ controller/page-others.js
@@ -0,0 +1,12 @@
+App.controller('page-faces', function (page) {
+	$(page).find('#button-other-1').click(function () {
+		kik.send({
+			title: '(۶ૈ ᵒ̌ Дᵒ̌)۶ૈ=͟͟͞͞ ⌨'
+		});
+	});
+	$(page).find('#button-other-2').click(function () {
+		kik.send({
+			title: '[̲̅$̲̅(̲̅5̲̅)̲̅$̲̅]'
+		});
+	});
+});
