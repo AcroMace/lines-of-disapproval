e86b0fcfba0a48cb08addbcbed2afc89dc0e752f
Andy <acromace@gmail.com>
2014-07-10 23:51:19 -0400

    Display a sorry text for Android users

--- web/controller/home.js
+++ web/controller/home.js
@@ -1,3 +1,10 @@
 App.controller('home', function (page) {
-	// put stuff here
+	var os = kik.utils.platform.os.name;
+	window.onload = function() {
+		if (os === 'android') {
+			document.getElementById("smiley-bar").innerHTML="You have an Android phone. A lot of the smileys will be broken, sorry!";
+		} else {
+			document.getElementById("smiley-bar").innerHTML="ᕕ( ᐛ )ᕗ";
+		}
+	};
 });
